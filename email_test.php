f907dc73200b026ad383c7213bcff1002f46e1b0
Hyujin12 <eugenedianito@gmai.com>
2025-10-22 03:40:12 +0800

    Commit

--- email_test.php
+++ email_test.php
@@ -1,33 +1,25 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-$apiKey = $_ENV['RESEND_API_KEY'];
+$apiKey = "YOUR_RESEND_API_KEY";
 $url = "https://api.resend.com/emails";
 
 $data = [
-  "from" => "Halili Dental Clinic <onboarding@resend.dev>",
-  "to" => ["your-email@gmail.com"],
-  "subject" => "Test Email",
-  "html" => "<h1>Testing Resend API from PHP</h1>"
+    "from" => "Halili Dental Clinic <no-reply@halilidentalclinic.shop>",
+    "to" => ["rayzeltelenr@gmail.com"],
+    "subject" => "Test Email",
+    "html" => "<p>This is a test from Halili Dental Clinic.</p>"
 ];
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
-  "Authorization: Bearer $apiKey",
-  "Content-Type: application/json"
+    "Authorization: Bearer $apiKey",
+    "Content-Type: application/json"
 ]);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
 $response = curl_exec($ch);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-$error = curl_error($ch);
 curl_close($ch);
 
-echo "HTTP Code: $httpCode\n";
-echo "Response: $response\n";
-echo "Error: $error\n";
+echo "HTTP: $httpCode\nResponse: $response\n";
